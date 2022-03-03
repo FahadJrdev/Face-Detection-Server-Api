660e1a549eaf4eb98a2b64c18cd849e50cd7ed0e
Fahadjrdev <anonnyaishan@gmail.com>
2022-03-03 13:25:00 +0600

    changing response

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 
-app.get('/', (req, res) => { res.send(database.users); })
+app.get('/', (req, res) => { res.send("It is working"); })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
